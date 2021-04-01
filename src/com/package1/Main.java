6a53408a9e2c3009ab79c3d96bc4c945504b7b13
vj17 <varsha.jayaraj94@gmail.com>
2021-04-01 16:49:19 +0530

    Added comments

--- src/com/package1/Main.java
+++ src/com/package1/Main.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
         StringCalculator calculator = new StringCalculator();
         calculator.Add("1,2");
         calculator.Add("34,56,1");
--- src/com/package1/StringCalculator.java
+++ src/com/package1/StringCalculator.java
@@ -8,18 +8,23 @@ public class StringCalculator {
 
     public int Add(String numbers) {
 
+        // to keep track of the number of times the Add method was called
         count++;
 
+        // if the string is empty, return 0
         if (numbers.equals(""))
             return 0;
 
+        // return sum
         return calculateSum(extractNumbers(numbers));
     }
 
+    // method to get the count of the number of times, Add was called
     public int GetCalledCount() {
         return count;
     }
 
+    // method to separate the numbers from the delimiters, "," and "\n"
     private String[] extractNumbers(String numbers) {
 
         String delimiter = null;
@@ -32,10 +37,14 @@ public class StringCalculator {
         return numbers.split("(,|\\n|" + delimiter + ")");
     }
 
+    // method to compute the sum of numbers after they are extracted
     private int calculateSum(String[] numbersArray) {
         int sum = 0;
+
+        // to store the negative numbers in the array
         ArrayList negativeNumbers = new ArrayList();
 
+        // add positive numbers, store the negative numbers in the array list
         for (String s : numbersArray) {
             int num = Integer.parseInt(s);
 
@@ -46,6 +55,7 @@ public class StringCalculator {
                 sum += num;
         }
 
+        // throw an exception if there are negative numbers
         if (negativeNumbers.size() > 0)
             throw new IllegalArgumentException("Negatives not allowed: " + negativeNumbers);
 
