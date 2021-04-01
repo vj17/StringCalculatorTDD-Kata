e60647d52d0fe0d00205bd56f7d5130fdda15f56
vj17 <varsha.jayaraj94@gmail.com>
2021-04-01 15:08:52 +0530

    Refactored code, extracted a method to calculate sum

--- src/com/package1/StringCalculator.java
+++ src/com/package1/StringCalculator.java
@@ -1,29 +1,31 @@
 package com.package1;
 
-import com.sun.security.jgss.GSSUtil;
-
-import java.util.Arrays;
-
 public class StringCalculator {
 
 
     public int Add(String numbers) {
 
-        String delimiter=null;
+        String delimiter = null;
 
-        if(numbers.equals(""))
+        if (numbers.equals(""))
             return 0;
 
-        if(numbers.contains("//")) {
+        if (numbers.contains("//")) {
             delimiter = numbers.substring(2, 3);
             numbers = numbers.substring(4);
         }
 
-        String[] numbersArray = numbers.split("(,|\\n|"+delimiter+")");
+        String[] numbersArray = numbers.split("(,|\\n|" + delimiter + ")");
 
+        int sum = calculateSum(numbersArray);
+
+        return sum;
+    }
+
+    private int calculateSum(String[] numbersArray) {
         int sum = 0;
 
-        for(String s: numbersArray)
+        for (String s : numbersArray)
             sum += Integer.parseInt(s);
 
         return sum;
